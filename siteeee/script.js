a720d1f5cb980ebe93fefaf4ef962277abb8e90c
yagojardimm <134665777+yagojardimm@users.noreply.github.com>
2024-06-25 18:15:27 -0300

    Update script.js

--- siteeee/script.js
+++ siteeee/script.js
@@ -4,24 +4,25 @@ function toggleMenu() {
 }
 
 // Carrossel de imagens
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function showSlides(n) {
     let i;
     const slides = document.getElementsByClassName("mySlides");
     const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 function plusSlides(n) {
@@ -31,3 +32,11 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+
+function autoShowSlides() {
+    slideIndex++;
+    showSlides(slideIndex);
+    setTimeout(autoShowSlides, 3000); // Muda a imagem a cada 3 segundos
+}
+
+autoShowSlides();
